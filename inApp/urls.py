43d824b13e7a51aacc4897a7341bc5e2332b9f34
Robert Santos <rasbms81608@gmail.com>
2020-10-28 14:54:43 -0500

    added the Admin path and connected the client data base to the admin url

--- inApp/urls.py
+++ inApp/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
+
+from inApp.models import Client as U
+class UAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(U, UAdmin)
+
+
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('', views.index),
     path('createClient', views.createClient),
     path('loginClient', views.loginClient),
     path('clientPage', views.clientPage),
 ]
+#  life.energy@att.net
+#  Sunnie69
